7e70d66e2a4dd390674f88cde56ec625e987ae05
toshi-konno <tkonno3169@gmail.com>
2018-09-24 00:22:39 +0900

    bingo game create

--- bingo_php/index.php
+++ bingo_php/index.php
@@ -1,24 +1,54 @@
+<?php
+
+/*
+$nums[][]
+B: $nums[0] 1-15
+I: $nums[1] 16-30
+..
+O: $nums[4]
+
+$nums[$i] $i * 15 + 1 〜 $i * 15 + 15
+*/
+
+$nums = [];
+
+for ($i = 0; $i < 5; $i++) {
+  $col = range($i * 15 + 1, $i * 15 + 15);
+  shuffle($col);
+  $nums[$i] = array_slice($col, 0, 5);
+}
+
+$nums[2][2] = "FREE";
+
+// var_dump($nums);
+// exit;
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>BINGO!</title>
-    <link rel="stylesheet" href="styles.css">
+  <meta charset="utf-8">
+  <title>BINGO!</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div id="container">
-        <table>
-            <tr>
-                <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-            </tr>
-        </table>
-    </div>
+  <div id="container">
+    <table>
+      <tr>
+        <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
+      </tr>
+      <?php for ($i = 0; $i < 5; $i++) : ?>
+      <tr>
+        <?php for ($j = 0; $j < 5; $j++) : ?>
+        <td><?= h($nums[$j][$i]); ?></td>
+        <?php endfor; ?>
+      </tr>
+      <?php endfor; ?>
+    </table>
+  </div>
 </body>
 </html>
